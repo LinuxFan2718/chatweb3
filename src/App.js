31cfb996b992ffec53165140ff311132785aa3b1
Dennis Cahillane <dennis@denniscahillane.info>
2023-02-01 21:04:55 -0500

    link to profile page

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
           ChatGPT is out of date. ChatWeb3 knows about the latest Web3 events!
         </p>
         <p>
-          A <a href="https://www.miamihackweek.com/">Miami Hack Week 2023</a> project by <a href="https://twitter.com/DennisLibre">Dennis</a> at <a href="https://www.daohouse.org/">DAO House</a>.
+          A <a href="https://www.miamihackweek.com/">Miami Hack Week 2023</a> project by <a href="https://cahillanelabs.com/profile-dennis">Dennis</a> at <a href="https://www.daohouse.org/">DAO House</a>.
         </p>
     </div>
   );
